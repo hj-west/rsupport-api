323ceaf8b8dfeff27c2da896e99ee54802c6f865
h.j_west <guswl9814@gmail.com>
2025-03-17 22:10:49 +0900

    User 클래스 오타 수정

--- src/main/java/com/rsupport/api/entity/User.java
+++ src/main/java/com/rsupport/api/entity/User.java
@@ -10,7 +10,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-class User {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
