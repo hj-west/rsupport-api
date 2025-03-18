991a382fbb792c0e557fe1af9fd8e3c744134685
h.j_west <guswl9814@gmail.com>
2025-03-18 18:02:24 +0900

    파일 삭제용 파일 서비스 메서드 생성

--- src/main/java/com/rsupport/api/service/FileServiceImpl.java
+++ src/main/java/com/rsupport/api/service/FileServiceImpl.java
@@ -3,6 +3,8 @@ package com.rsupport.api.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -26,4 +28,19 @@ public class FileServiceImpl implements FileService {
             throw new RuntimeException("파일 저장 실패", e);
         }
     }
+
+    @Override
+    public void delete(String filePath) {
+        if (filePath == null || filePath.isEmpty()) {
+            throw new IllegalArgumentException("파일 경로가 비어 있습니다.");
+        }
+
+        File file = new File(filePath);
+        if (file.exists()) {
+            if (!file.delete()) {
+                throw new RuntimeException("파일 삭제 실패: " + filePath);
+            }
+        }
+    }
+
 }
