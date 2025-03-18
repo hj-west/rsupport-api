d3512e604ea10a2c6618a7597e837802a8acddba
h.j_west <guswl9814@gmail.com>
2025-03-18 16:33:20 +0900

    공지 목록 조회 API 생성

--- src/main/java/com/rsupport/api/controller/NoticeController.java
+++ src/main/java/com/rsupport/api/controller/NoticeController.java
@@ -1,20 +1,51 @@
 package com.rsupport.api.controller;
 
+import com.rsupport.api.dto.NoticeListResponseDto;
 import com.rsupport.api.dto.NoticeRequestDto;
+import com.rsupport.api.dto.enums.SearchType;
 import com.rsupport.api.dto.validation.RegisterRequestValidationGroup;
 import com.rsupport.api.service.NoticeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/notices")
 @RequiredArgsConstructor
 class NoticeController {
     private final NoticeService noticeService;
+
+    /**
+     * 공지사할 목록 조회 API
+     * @param searchType 검색 타입
+     * @param keyword 검색어
+     * @param from 조회시작일
+     * @param to 조회종료일
+     * @param page 페이지번호
+     * @param size 페이지당 공지글 갯수
+     * @param sort 정렬값 조건
+     * @return ResponseEntity<Page<NoticeListResponseDto>>
+     */
+    @GetMapping
+    public ResponseEntity<Page<NoticeListResponseDto>> getNotices(@RequestParam(required = false) SearchType searchType,
+                                                                  @RequestParam(required = false) String keyword,
+                                                                  @RequestParam(required = false) LocalDateTime from,
+                                                                  @RequestParam(required = false) LocalDateTime to,
+                                                                  @RequestParam(defaultValue = "0") int page,
+                                                                  @RequestParam(defaultValue = "10") int size,
+                                                                  @RequestParam(defaultValue = "createdAt") String sort) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, sort));
+        return ResponseEntity.ok(noticeService.getNotices(searchType, keyword, from, to, pageable));
+    }
 
     /**
      * 공지사항 저장 API
